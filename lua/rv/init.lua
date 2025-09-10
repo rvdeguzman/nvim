4b9cb4b943afc121e7c1a7d36f9fbeed9ce7b1bb
rvdeguzman <rvdeguzman@protonmail.com>
2025-09-10 19:53:45 -0400

    transparency

--- lua/rv/init.lua
+++ lua/rv/init.lua
@@ -15,9 +15,9 @@ vim.keymap.set('n', '<leader>wv', '<cmd>vsplit<CR>')
 vim.keymap.set('n', '<leader>wd', '<cmd>close<CR>')
 
 
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to clipboard' })
-vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Yank line to clipboard' })
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from clipboard' })
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = 'Paste from clipboard before' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set({ 'n', 'v' }, '<leader>p')
+vim.keymap.set({ 'n', 'v' }, '<leader>P')
 
 require('rv.lazy_init')
