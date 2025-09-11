95acca18dfeeb24d81e147fd13516ba37c29e0a4
rvdeguzman <rvdeguzman@protonmail.com>
2025-09-11 13:37:03 -0400

    lua stuff

--- lua/rv/plugins/lsp.lua
+++ lua/rv/plugins/lsp.lua
@@ -240,18 +240,12 @@ return {
         config = function()
             require('conform').setup({
                 formatters_by_ft = {
-                    -- Conform will run multiple formatters sequentially
-                    python = { "isort", "black" },
-                    -- You can customize some of the format options for the filetype (:help conform.format)
-                    -- Conform will run the first available formatter
-                    javascript = { "prettierd", "prettier", stop_after_first = true },
                 },
                 format_on_save = {
                     timeout_ms = 500,
                     lsp_format = 'fallback'
                 },
                 formatters = {
-                    -- formatter configs
                 }
             })
         end,
