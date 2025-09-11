df179c7cad86e6548efaf1df8a2e07029623aca7
rvdeguzman <rvdeguzman@protonmail.com>
2025-09-11 13:51:27 -0400

    formatting

--- lua/rv/init.lua
+++ lua/rv/init.lua
@@ -15,9 +15,9 @@ vim.keymap.set('n', '<leader>wv', '<cmd>vsplit<CR>')
 vim.keymap.set('n', '<leader>wd', '<cmd>close<CR>')
 
 
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to clipboard' })
 vim.keymap.set('n', '<leader>Y', '"+Y', { desc = 'Yank line to clipboard' })
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from clipboard' })
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', { desc = 'Paste from clipboard before' })
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = 'Paste from clipboard before' })
 
 require('rv.lazy_init')
