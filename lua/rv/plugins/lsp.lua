de90c1fea911d6be59707a193e9e1d60c4f85152
Rafael Vincent <rv@lisbon.local>
2026-01-24 11:42:50 -0500

    fix: treesitter

--- lua/rv/plugins/lsp.lua
+++ lua/rv/plugins/lsp.lua
@@ -134,29 +134,27 @@ return {
             "nvim-treesitter/nvim-treesitter-context",
         },
         config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = {
-                    'lua', 'vim', 'vimdoc',
-                    'javascript', 'typescript',
-                    'c', 'cpp',
-                    'python', 'html', 'css',
-                    'markdown', 'markdown_inline',
-                },
-                highlight = { enable = true, additional_vim_regex_highlighting = false },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = '<CR>',
-                        node_incremental = '<CR>',
-                        node_decremental = '<BS>',
-                    },
-                },
+            -- Install parsers
+            local ensure_installed = {
+                'lua', 'vim', 'vimdoc',
+                'javascript', 'typescript',
+                'c', 'cpp',
+                'python', 'html', 'css',
+                'markdown', 'markdown_inline',
+            }
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "LazyDone",
+                once = true,
+                callback = function()
+                    for _, lang in ipairs(ensure_installed) do
+                        pcall(vim.treesitter.language.add, lang)
+                    end
+                end,
             })
 
-            -- treesitter folding
+            -- treesitter folding (use built-in vim.treesitter.foldexpr)
             vim.opt.foldmethod = 'expr'
-            vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+            vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
             vim.opt.foldlevel = 99
             vim.opt.foldlevelstart = 99
             vim.opt.foldenable = true
