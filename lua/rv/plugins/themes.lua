67c1167b3f0a9c2a1697efcf0be7ae5f2107add1
rv <rvdeguzman@protonmail.com>
2026-02-17 16:33:13 -0500

    added transparent theme

--- lua/rv/plugins/themes.lua
+++ lua/rv/plugins/themes.lua
@@ -1,8 +1,12 @@
 return {
-    "xero/miasma.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd("colorscheme miasma")
-    end,
+	"xero/miasma.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme miasma")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	end,
 }
