b1767251ca28f3b67bcd92c0f906b66ab89d0379
rv <rvdeguzman@protonmail.com>
2026-03-02 23:30:58 -0500

    oil

--- lua/rv/plugins/themes.lua
+++ lua/rv/plugins/themes.lua
@@ -1,23 +1,7 @@
-return
--- {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		vim.cmd("colorscheme rose-pine")
--- 	end,
--- }
---
---
---
-{
-	"xero/miasma.nvim",
-	lazy = false,
-	priority = 1000,
+return {
+	"morhetz/gruvbox",
 	config = function()
-		vim.cmd("colorscheme miasma")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.opt.background = "dark"
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
